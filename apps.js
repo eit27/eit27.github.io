69b9a99f7d82a7ba09e42bb0c858a645beda51d2
eit27 <149132247+eit27@users.noreply.github.com>
2024-10-14 22:58:10 -0700

    Add files via upload

--- apps.js
+++ apps.js
@@ -1,5 +1,5 @@
 console.log('Hello World!')
-const name = "Evelyn"
+
 let hasDownloadedResume = false;
 let date1 = new Date ("10/14/2024");
 let date2 = new Date ("12/26/2024");
@@ -18,18 +18,10 @@ function showGreeting(name)
     return("Hello, my name is " + name +"! Welcome to my portfolio!")
 }
 
-window.onload = function() 
-{
-
-    const greetingMessage = showGreeting();
-    document.getElementById("greeting").innerText = greetingMessage;
-};
-
-
 
 
 function daysUntilDeadline(date1, date2) {
-
+    
     let Difference_In_Time = date2.getTime() - date1.getTime();
     let Difference_In_Days = Math.round(Difference_In_Time / (1000 * 3600 * 24));
 
@@ -44,6 +36,10 @@ window.onload = function() {
     const showDeadline = daysUntilDeadline(date1, date2);
     
     document.getElementById("deadline").innerText = showDeadline;
+    const name = "Evelyn"
+    const showMessage = showGreeting(name);
+    
+    document.getElementById("greeting").innerText = showMessage;
 };
 
 
